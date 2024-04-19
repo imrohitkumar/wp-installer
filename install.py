83e5f5273a81387367a882cb9039cc94f32f5afb
Rohit Kumar <46277392+imrohitkumar@users.noreply.github.com>
2024-04-20 03:13:03 +0530

    Update install.py

--- install.py
+++ install.py
@@ -1,7 +1,5 @@
 import subprocess
 import os
-import http.server
-import socketserver
 
 # Define the packages to install
 packages = ["nginx", "mysql80-server", "wordpress", "php82"]
@@ -16,121 +14,61 @@ print("Configuring MySQL...")
 subprocess.run(["sysrc", "mysql_enable=YES"])
 subprocess.run(["service", "mysql-server", "start"])
 
-# Configure PHP
-print("Configuring PHP...")
-subprocess.run(["sysrc", "php_fpm_enable=YES"])
-subprocess.run(["service", "php-fpm", "start"])
+# Get database credentials from user
+mysql_root_password = input("Enter MySQL root password: ")
+wordpress_db_name = input("Enter WordPress database name: ")
+wordpress_db_user = input("Enter WordPress database user: ")
+wordpress_db_password = input("Enter WordPress database password: ")
+
+# Configure MySQL database
+print("Configuring MySQL database...")
+subprocess.run(["mysql", "-uroot", "-e", f"CREATE DATABASE {wordpress_db_name};"])
+subprocess.run(["mysql", "-uroot", "-e", f"GRANT ALL PRIVILEGES ON {wordpress_db_name}.* TO '{wordpress_db_user}'@'localhost' IDENTIFIED BY '{wordpress_db_password}';"])
+subprocess.run(["mysql", "-uroot", "-e", "FLUSH PRIVILEGES;"])
+
+# Configure WordPress
+print("Configuring WordPress...")
+wordpress_config = """
+<?php
+define('DB_NAME', '{wordpress_db_name}');
+define('DB_USER', '{wordpress_db_user}');
+define('DB_PASSWORD', '{wordpress_db_password}');
+define('DB_HOST', 'localhost');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+?>
+""".format(wordpress_db_name=wordpress_db_name, wordpress_db_user=wordpress_db_user, wordpress_db_password=wordpress_db_password)
+with open("/usr/local/www/wordpress/wp-config.php", "w") as f:
+    f.write(wordpress_config)
 
 # Configure Nginx
 print("Configuring Nginx...")
-subprocess.run(["sysrc", "nginx_enable=YES"])
-subprocess.run(["service", "nginx", "start"])
+nginx_config = """
+server {
+    listen 80;
+    server_name localhost;
 
-# Create a setup wizard
-class SetupWizardHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        if self.path == "/":
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            html = """
-            <html>
-            <body>
-            <h1>WordPress Setup Wizard</h1>
-            <form action="/setup" method="post">
-            <label for="mysql_root_password">MySQL Root Password:</label>
-            <input type="password" id="mysql_root_password" name="mysql_root_password">
+    root /usr/local/www/wordpress;
+    index index.php index.html index.htm;
 
+    location / {
+        try_files $uri $uri/ /index.php?q=$uri&$args;
+    }
 
-            <label for="wordpress_db_name">WordPress Database Name:</label>
-            <input type="text" id="wordpress_db_name" name="wordpress_db_name">
+    location ~ \.php$ {
+        try_files $uri =404;
+        fastcgi_pass 127.0.0.1:9000;
+        fastcgi_param SCRIPT_FILENAME $request_filename;
+        include fastcgi_params;
+    }
+}
+"""
+with open("/usr/local/etc/nginx/nginx.conf", "w") as f:
+    f.write(nginx_config)
 
+# Enable PHP in Nginx
+print("Enabling PHP in Nginx...")
+subprocess.run(["sysrc", "php_fpm_enable=YES"])
+subprocess.run(["service", "php-fpm", "start"])
 
-            <label for="wordpress_db_user">WordPress Database User:</label>
-            <input type="text" id="wordpress_db_user" name="wordpress_db_user">
-
-
-            <label for="wordpress_db_password">WordPress Database Password:</label>
-            <input type="password" id="wordpress_db_password" name="wordpress_db_password">
-
-
-            <input type="submit" value="Setup">
-            </form>
-            </body>
-            </html>
-            """
-            self.wfile.write(html.encode())
-        else:
-            http.server.SimpleHTTPRequestHandler.do_GET(self)
-
-    def do_POST(self):
-        if self.path == "/setup":
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            content_length = int(self.headers['Content-Length'])
-            post_data = self.rfile.read(content_length).decode()
-            post_data = post_data.split("&")
-            mysql_root_password = post_data[0].split("=")[1]
-            wordpress_db_name = post_data[1].split("=")[1]
-            wordpress_db_user = post_data[2].split("=")[1]
-            wordpress_db_password = post_data[3].split("=")[1]
-
-            # Configure MySQL
-            subprocess.run(["mysql", "-uroot", "-e", f"CREATE DATABASE {wordpress_db_name};"])
-            subprocess.run(["mysql", "-uroot", "-e", f"GRANT ALL PRIVILEGES ON {wordpress_db_name}.* TO '{wordpress_db_user}'@'localhost' IDENTIFIED BY '{wordpress_db_password}';"])
-            subprocess.run(["mysql", "-uroot", "-e", "FLUSH PRIVILEGES;"])
-
-            # Configure WordPress
-            wordpress_config = """
-            <?php
-            define('DB_NAME', '{wordpress_db_name}');
-            define('DB_USER', '{wordpress_db_user}');
-            define('DB_PASSWORD', '{wordpress_db_password}');
-            define('DB_HOST', 'localhost');
-            define('DB_CHARSET', 'utf8');
-            define('DB_COLLATE', '');
-          ?>""".format(wordpress_db_name=wordpress_db_name, wordpress_db_user=wordpress_db_user, wordpress_db_password=wordpress_db_password)
-            with open("/usr/local/www/wordpress/wp-config.php", "w") as f:
-                f.write(wordpress_config)
-
-            # Configure Nginx
-            nginx_config = """
-            server {
-                listen 80;
-                server_name localhost;
-
-                root /usr/local/www/wordpress;
-                index index.php index.html index.htm;
-
-                location / {
-                    try_files $uri $uri/ /index.php?q=$uri&$args;
-                }
-
-                location ~ \.php$ {
-                    try_files $uri =404;
-                    fastcgi_pass 127.0.0.1:9000;
-                    fastcgi_param SCRIPT_FILENAME $request_filename;
-                    include fastcgi_params;
-                }
-            }
-            """
-            with open("/usr/local/etc/nginx/nginx.conf", "w") as f:
-                f.write(nginx_config)
-
-            html = """
-            <html>
-            <body>
-            <h1>Setup Complete!</h1>
-            <p>WordPress is now available at <a href="http://localhost">http://localhost</a></p>
-            </body>
-            </html>
-            """
-            self.wfile.write(html.encode())
-        else:
-            http.server.SimpleHTTPRequestHandler.do_POST(self)
-
-# Start the setup wizard
-httpd = socketserver.TCPServer(("", 8000), SetupWizardHandler)
-print("Setup Wizard available at http://localhost:8000")
-httpd.serve_forever()
+print("WordPress is now available at http://localhost/wp-admin/install.php")
