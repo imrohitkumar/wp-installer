8652d34c6e6f0973d8e6c40c69b9860db13a5bec
Rohit Kumar <46277392+imrohitkumar@users.noreply.github.com>
2024-04-20 12:24:53 +0530

    Update install.py

--- install.py
+++ install.py
@@ -1,10 +1,7 @@
 import subprocess
-import os
-import tarfile
-import shutil
 
 # Define the packages to install
-packages = ["nginx", "mysql80-server", "php82"]
+packages = ["nginx", "mysql80-server", "wordpress", "php82"]
 
 # Install the packages
 for package in packages:
@@ -28,25 +25,9 @@ subprocess.run(["mysql", "-uroot", "-e", f"CREATE DATABASE {wordpress_db_name};"
 subprocess.run(["mysql", "-uroot", "-e", f"GRANT ALL PRIVILEGES ON {wordpress_db_name}.* TO '{wordpress_db_user}'@'localhost' IDENTIFIED BY '{wordpress_db_password}';"])
 subprocess.run(["mysql", "-uroot", "-e", "FLUSH PRIVILEGES;"])
 
-# Download and extract WordPress installation archive
-print("Downloading and extracting WordPress installation archive...")
-wordpress_archive_url = "https://wordpress.org/latest.tar.gz"
-wordpress_archive_file = "/tmp/wordpress.tar.gz"
-subprocess.run(["fetch", "-o", wordpress_archive_file, wordpress_archive_url])
-with tarfile.open(wordpress_archive_file, 'r:gz') as tar:
-    tar.extractall(path='/tmp/')
-
-# Move WordPress installation to /usr/local/www/
-print("Moving WordPress installation to /usr/local/www/...")
-shutil.move('/tmp/wordpress', '/usr/local/www/wordpress')
-
-# Set permissions for WordPress installation
-print("Setting permissions for WordPress installation...")
-subprocess.run(["chown", "-R", "www:www", "/usr/local/www/wordpress"])
-
 # Configure WordPress
 print("Configuring WordPress...")
-wordpress_config = """
+wordpress_config = f"""
 <?php
 define('DB_NAME', '{wordpress_db_name}');
 define('DB_USER', '{wordpress_db_user}');
@@ -58,7 +39,7 @@ define('WP_CONTENT_DIR', 'wp-content');
 define('WP_PLUGIN_DIR', 'wp-content/plugins');
 define('WP_UPLOADS_DIR', 'wp-content/uploads');
 ?>
-""".format(wordpress_db_name=wordpress_db_name, wordpress_db_user=wordpress_db_user, wordpress_db_password=wordpress_db_password)
+"""
 with open("/usr/local/www/wordpress/wp-config.php", "w") as f:
     f.write(wordpress_config)
 
@@ -92,10 +73,6 @@ http {
 """
 with open("/usr/local/etc/nginx/nginx.conf", "w") as f:
     f.write(nginx_config)
-
-# Set permissions for Nginx configuration
-print("Setting permissions for Nginx configuration...")
-subprocess.run(["chown", "root:wheel", "/usr/local/etc/nginx/nginx.conf"])
 
 # Enable PHP in Nginx
 print("Enabling PHP in Nginx...")
